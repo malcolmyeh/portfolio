fc4f46615399848b9d5873ff29f1e568065ba0ba
malcolmyeh <malcolmyeh@gmail.com>
2019-12-30 22:57:34 -0500

    icon fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons"
+import { Row, Col } from "reactstrap";
+
+const SocialList = [
+    {
+        link: "https://www.github.com/malcolmyeh",
+        icon: faGithub
+    },
+    {
+        link: "https://www.linkedin.com",
+        icon: faLinkedin
+    },
+    {
+        link: "https://instagram.com",
+        icon: faInstagram
+    }
+];
 
 const FooterContainer = styled.div`
     width: 100%;
@@ -14,7 +32,19 @@ const FooterContainer = styled.div`
 
 const Footer = () => (
     <FooterContainer>
+        <div style={{width: "20%", margin: "0 auto"}}>
         <p>Copyright Malcolm Yeh 2019</p>
+        <Row>
+            {SocialList.map(social => (
+                <Col>
+                    <a style={{ color: "white" }} href={social.link}>
+                        <FontAwesomeIcon icon={social.icon} />
+                    </a>
+                </Col>
+
+            ))}
+        </Row>
+        </div>
     </FooterContainer>
 );
 
